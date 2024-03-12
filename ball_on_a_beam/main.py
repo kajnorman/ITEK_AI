f6f964b4da72a2622c85d011b262b7c5110bf1b3
kajnorman <knn@eaaa.dk>
2024-03-12 21:37:07 +0100

    Create main.py

---
+++ ball_on_a_beam/main.py
@@ -0,0 +1,12 @@
+from boabvissim import init,run
+import time
+
+
+
+init()  # denne linje initierer den animerede maskine  standsemaskine.
+
+
+
+
+while True:
+    run() # denne linje skal indgå i while løkken for at animere maskinen standsemaskine.
